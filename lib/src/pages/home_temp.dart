efbdc8288c93b5ca8a90efc3ed847cecad00232f
Marcos E. Herrera <marcosherrera.ib@gmail.com>
2021-05-23 23:17:22 -0300

    Se hizo uso de los widgets ListView and ListTile.

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -3,8 +3,26 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Temp'),   
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('ListTile Title 1'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Title 2'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('ListTile Title 3'),
+          ),
+          Divider()
+        ],
+      ),
     );
   }
 }
